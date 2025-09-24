3ffafba5897d8a62efaa0d59d80ca07a32fb1ee7
salma hesham <162762766+salmahesha@users.noreply.github.com>
2025-09-24 17:07:03 +0300

    done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://ecommerce.routemisr.com',
-  domain: 'https://salmahesha.github.io/E-commerce'
+  domain: 'https://salmahesha.github.io/E-commerce/allorders'
 };
