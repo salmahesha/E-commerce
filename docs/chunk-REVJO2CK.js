723cc0eddaab151a0c2c3c875fa549bac7f6891b
salma hesham <162762766+salmahesha@users.noreply.github.com>
2025-10-08 15:16:46 +0300

    done

---
+++ docs/chunk-REVJO2CK.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-37SS7SX3.js";import"./chunk-TPVZDMZP.js";import"./chunk-4CLCTAJ7.js";export{a as MyAccountComponent};
--- docs/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'https://ecommerce.routemisr.com',
-  domain: 'https://salmahesha.github.io/E-commerce'
-
-};
